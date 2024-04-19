1b8574275e9f195ef3b7801f06ce36906225405e
Mohith Surnam <mohithrocky40@gmail.com>
2024-04-19 12:32:04 +0530

    Implement login,logout endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -46,9 +46,42 @@ export const signup=async (req,res)=>{
     }
     
 }
-export const login=(req,res)=>{
-    console.log("login user");
+
+export const login=async(req,res)=>{
+    try {
+        const {username,password}=req.body;
+        const user=await User.findOne({username});
+        const iscorrectPassword=await bcrypt.compare(password,user?.password ||"")
+
+        if(!user || !iscorrectPassword){
+            return res.json({error:"Invalid username or password"});
+        }
+
+        generateToken(user._id,res);
+        res.status(200).json({
+            _id:user._id,
+            fullname:user.fullname,
+            username:user.username,
+            profilePic:user.profilePic
+        })
+
+
+    } catch (error) {
+        console.log("Error in login controller",error.message);
+        res.status(500).json({
+            error:"Internal errror"
+        })
+    }
 }
+
 export const logout=(req,res)=>{
-    console.log("logout user");
+    try {
+        res.cookie("jwt","",{maxAge:0});
+        res.status(200).json({message:"Logged out successfully"});
+    } catch (error) {
+        console.log("Error in logout controller",error.message);
+        res.status(500).json({
+            error:"Internal errror"
+        })
+    }
 }
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,7 +4,7 @@ const generateToken=(userId,res)=>{
 const token =jwt.sign({userId},process.env.JWT_SECRET,{
     expiresIn:"15d"
 });
-res.cookie("jwt","token",{
+res.cookie("jwt",token,{
     maxAge:15*24*60*60*1000,
     httpOnly:true,
     sameSite:"strict",
